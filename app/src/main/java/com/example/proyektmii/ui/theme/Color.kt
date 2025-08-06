5e335c54ee1d9ab8148fc0d4cf5688a80dcc37d9
dheaasprla <158927646+dheaasprla@users.noreply.github.com>
2025-08-06 09:55:20 +0700

    tambahin color

--- app/src/main/java/com/example/proyektmii/ui/theme/Color.kt
+++ app/src/main/java/com/example/proyektmii/ui/theme/Color.kt
@@ -9,3 +9,11 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+// Tambahkan palet warna TMII
+val ColorPrimary = Color(0xFFE10600) // Header, sidebar
+val ColorBackground = Color(0xFFFFFFFF) // Background, teks utama
+val ColorOutline = Color(0xFFBBBBBB) // Outline, garis pemisah
+val ColorAccent = Color(0xFF005BAC) // Aksen, tombol penting
+val ColorWarning = Color(0xFFE76F51) // Status penting (peringatan, habis)
+val ColorPositive = Color(0xFFFFD100) // Status positif (tersedia, sukses)
